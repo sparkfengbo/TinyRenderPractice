d16b3fb27547739cf445a6365147fe4d45e120af
冯博 <fengbo.hit@bytedance.com>
2020-05-01 23:06:54 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TinyRenderForMyOwn.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TinyRenderForMyOwn.dir/main.cpp.o"
+  "CMakeFiles/TinyRenderForMyOwn.dir/tgaimage.cpp.o"
+  "TinyRenderForMyOwn"
+  "TinyRenderForMyOwn.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TinyRenderForMyOwn.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
