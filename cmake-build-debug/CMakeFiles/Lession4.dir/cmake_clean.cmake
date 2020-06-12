afc45f056969a8e073bc85f26a3ce99cf085ce99
fengbo.hit <fengbo.hit@bytedance.com>
2020-06-12 13:32:45 +0800

    lession4

---
+++ cmake-build-debug/CMakeFiles/Lession4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lession4.dir/L4_PerspectiveProjection.cpp.o"
+  "CMakeFiles/Lession4.dir/geometry.cpp.o"
+  "CMakeFiles/Lession4.dir/model.cpp.o"
+  "CMakeFiles/Lession4.dir/tgaimage.cpp.o"
+  "Lession4"
+  "Lession4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lession4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- geometry.h
+++ geometry.h
@@ -63,4 +63,28 @@ template <class t> std::ostream& operator<<(std::ostream& s, Vec3<t>& v) {
 }
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+const int DEFAULT_ALLOC=4;
+
+class Matrix {
+    std::vector<std::vector<float> > m;
+    int rows, cols;
+public:
+    Matrix(int r=DEFAULT_ALLOC, int c=DEFAULT_ALLOC);
+    inline int nrows();
+    inline int ncols();
+
+    static Matrix identity(int dimensions);
+    std::vector<float>& operator[](const int i);
+    Matrix operator*(const Matrix& a);
+    Matrix transpose();
+    Matrix inverse();
+
+    friend std::ostream& operator<<(std::ostream& s, Matrix& m);
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+
 #endif //TINYRENDERPRACTICE_GEOMETRY_H
